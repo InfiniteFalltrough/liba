8fc6f8f935b649ec1b1c2c484715124ef3902419
#vik <golubenkovvs@gmail.com>
2025-02-21 19:11:27 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "liba",
-            url: "",
+            name: "HevSocks5Tunnel",
+            url: "https://github.com/InfiniteFalltrough/liba/releases/download/v0.0.1/HevSocks5Tunnel.xcframework.zip",
             checksum: "d8b0b6d0493b33fb7243be04dbe652732042e030f6ebef505b32872268fbd43f"
         )
     ]
